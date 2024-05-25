6586c5023932ca0fc944134bef5402f9074f6757
AlexeyAleksandrov <chinok73@yandex.ru>
2024-05-25 14:35:56 +0300

    Init project

---
+++ src/main/java/io/github/alexeyaleksandrov/jacademicsupprt/JAcademicSupprtApplication.java
@@ -0,0 +1,15 @@
+package io.github.alexeyaleksandrov.jacademicsupprt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JAcademicSupprtApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JAcademicSupprtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/alexeyaleksandrov/jacademicsupprt/JAcademicSupprtApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.alexeyaleksandrov.jacademicsupprt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JAcademicSupprtApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
