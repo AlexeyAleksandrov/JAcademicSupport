0b4a8519e4c4f2a30d4f22a8dbd7e72ef786e490
AlexeyAleksandrov <chinok73@yandex.ru>
2024-05-27 22:52:05 +0300

    Сделано получение списка вакансий с HH

--- src/main/java/io/github/alexeyaleksandrov/jacademicsupport/services/hh/HhWebClient.java
+++ src/main/java/io/github/alexeyaleksandrov/jacademicsupport/services/hh/HhWebClient.java
@@ -7,50 +7,80 @@ import io.github.alexeyaleksandrov.jacademicsupport.models.hh.Vacancy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class HhWebClient {
     public List<Vacancy> getAllVacancies(String searchText) {
-        searchText = searchText.replace(" ", "%20");
-        String url = "https://api.hh.ru/vacancies?text=" + searchText +"&area=1";
-//        String url = "https://api.hh.ru/vacancies?text=juniur%20java%20deleveloper&area=1";
-        System.out.println(url);
 
-        WebClient.Builder webClientBuilder = WebClient.builder();
+        String responseText = null;
 
-        WebClient webClient = WebClient.builder()
-                .defaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64);")
-                .build();
-        String responseText = webClient.get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        try {
+            URL url = new URL("https://api.hh.ru/vacancies?text=juniur%20java%20deleveloper&area=1");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
 
-        System.out.println(responseText);
-        System.out.println(responseText.length());
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response.toString());
+            httpURLConnection.disconnect();
+
+            responseText = response.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        searchText = searchText.replace(" ", "%20");
+//        String url = "https://api.hh.ru/vacancies?text=" + searchText +"&area=1";
+////        String url = "https://api.hh.ru/vacancies?text=juniur%20java%20deleveloper&area=1";
+//        System.out.println(url);
+//
+//        WebClient.Builder webClientBuilder = WebClient.builder();
+//
+//        WebClient webClient = WebClient.builder()
+//                .defaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64);")
+//                .build();
+//        String responseText = webClient.get()
+//                .uri(url)
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .block();
+//
+//        System.out.println(responseText);
+//        System.out.println(responseText.length());
 
         List<Vacancy> vacancies = new ArrayList<>();
 
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode jsonNode = null;
-//        try {
-//            jsonNode = objectMapper.readTree(responseText);
-//            JsonNode items = jsonNode.get("items");
-//            if(items.isArray()) {
-//                for (JsonNode itemNode : items) {
-//                    Vacancy vacancy = new Vacancy();
-//                    vacancy.setId(itemNode.get("id").asInt());
-//                    vacancy.setName(itemNode.get("name").asText());
-//                    vacancies.add(vacancy);
-//                }
-//            }
-//
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = objectMapper.readTree(responseText);
+            JsonNode items = jsonNode.get("items");
+            if(items.isArray()) {
+                for (JsonNode itemNode : items) {
+                    Vacancy vacancy = new Vacancy();
+                    vacancy.setId(itemNode.get("id").asInt());
+                    vacancy.setName(itemNode.get("name").asText());
+                    vacancies.add(vacancy);
+                }
+            }
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         return vacancies;
 
